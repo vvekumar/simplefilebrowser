0d07795ec066be15859491e72afc727265e24e29
Vivek Kumar <vivekk_28@apple.com>
2021-08-07 23:48:06 -0700

    Various edits - Updates to API - Added Docker - Added Documentation

--- simplefilebrowser/exceptions.py
+++ simplefilebrowser/exceptions.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
-"""
-Created by Vivek (Siri) Kumar on 7/31/21
-"""
-class Error(Exception):
-    def __init__(self,):
-        """Constructor for Error"""
 
 class PathIsNotFile(Exception):
-    pass
-
-class AuthenticationException(Exception):
     pass
--- test/test_api.py
+++ test/test_api.py
@@ -2,17 +2,32 @@
 Created by Vivek Kumar on 7/27/21
 """
 import unittest
-from simplefilebrowser.browser import filebrowser as fb
+from simplefilebrowser.api import app
 
 TEST_PATH_VALID = "//test"
 TEST_PATH_INVALID = "/Users/vivekk_28/Vi/code/playground/github/simple-file-browsers"
 TEST_FILE_PATH = "/Users/vivekk_28/Vi/code/playground/github/simplefilebrowser/simplefilebrowser/exceptions.py"
 
+class TestSFBAPI(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.api = app.SimpleFileBrowserAPI()
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    # def test_valid_root(self):
+    #     path = TEST_PATH_VALID
+    #     contents = TestFileBrowser.file_browser.show_dir(path)
+    #     self.assertIsNotNone(contents)
 
+    # def test_invalid_root(self):
+    #     path = TEST_PATH_INVALID
+    #     with self.assertRaises(FileExistsError):
+    #         TestFileBrowser.file_browser.show_dir(path)
+
+    # def test_show_file_contents(self):
+    #     path = TEST_FILE_PATH
+    #     contents = TestFileBrowser.file_browser.show_file(path)
+    #     print(contents)
+    #     assert len(contents) > 0
 
 if __name__ == '__main__':
     unittest.main()
