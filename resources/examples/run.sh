a0f2d2ffe5abc73165a578a2de246f5f40df39c8
Vivek Kumar <vivekk_28@apple.com>
2021-08-24 15:04:45 -0700

    fix run.sh

--- resources/examples/run.sh
+++ resources/examples/run.sh
@@ -10,6 +10,11 @@ ROOT_DIR=$MNT_LOC
 RELPATH=
 
 # save curl output to resp.json
-curl -o resp.json -u ${USERNAME}:${PASSWORD} 0.0.0.0:5000/sfb/api?root=${ROOT_DIR}&relpath=${RELPATH}
-
+curl -o resp.json -u ${USERNAME}:${PASSWORD} "0.0.0.0:5000/sfb/api?root=${ROOT_DIR}&relpath=${RELPATH}"
+sleep 2
 less resp.json | python -m json.tool
+
+# show request URI info
+echo "Showing request info: "
+docker logs sfb-api 1>/dev/null 2> debug.log
+tail -1 debug.log
